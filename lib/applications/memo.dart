10be29500c2edf8b1d404cd4ee5356625f0883b9
SakumaTakuya <mo-takusan9922@docomo.ne.jp>
2021-02-11 21:48:19 +0900

    fix application

--- lib/applications/memo.dart
+++ lib/applications/memo.dart
@@ -1,11 +1,17 @@
 import '../domains/memo.dart' show Memo;
-import '../domains/model.dart' show ModelSaver, ModelSearcher, QueryFunction;
+import '../domains/model.dart'
+    show ModelDeleter, ModelSaver, ModelSearcher, QueryFunction;
 
 class MemoFacade<M extends Memo> {
   MemoFacade(this._saver, M memo) : _memo = memo;
 
   final ModelSaver<M> _saver;
   M _memo;
+
+  get key => _memo.key;
+  get dateTime => _memo.dateTime;
+  get title => _memo.title;
+  get text => _memo.text;
 
   set dateTime(DateTime value) => _memo.dateTime = value;
   set title(String value) => _memo.title = value;
@@ -16,11 +22,19 @@ class MemoFacade<M extends Memo> {
   }
 }
 
-class MemoFacadeFactory<M extends Memo> {
-  const MemoFacadeFactory(this._searcher, this._saver, this._template);
+class MemoFacadeProvider<M extends Memo> {
+  const MemoFacadeProvider(
+      this._searcher, this._saver, this._deleter, this._template);
   final ModelSearcher<M> _searcher;
   final ModelSaver<M> _saver;
+  final ModelDeleter<M> _deleter;
   final M _template;
+
+  void delete(MemoFacade<M> data) {
+    if (data.key != null) {
+      _deleter.delete(data.key);
+    }
+  }
 
   MemoFacade<M> searchOrCreate(int key) {
     final memo = _searcher.search(key);
@@ -28,8 +42,15 @@ class MemoFacadeFactory<M extends Memo> {
       return MemoFacade<M>(_saver, memo);
     }
 
+    return _create(key: key);
+  }
+
+  MemoFacade<M> create() => _create();
+
+  MemoFacade<M> _create({int key}) {
     final copied = _template.copy() as M;
     if (copied != null) {
+      copied.key = key;
       return MemoFacade<M>(_saver, copied);
     }
     throw 'Templete type is not matched';
